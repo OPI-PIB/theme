ba313defb61db3eb5da73ba07e69f2904ac5c8b8
Dawid Lubowiecki <lubowiecki@gmail.com>
2025-05-28 08:28:24 +0200

    build

--- src/stories/components/forms/input.component.ts
+++ src/stories/components/forms/input.component.ts
@@ -4,8 +4,15 @@ import { Component } from "@angular/core";
   selector: "app-input",
   imports: [],
   template: `
-    <label for="inputId">Input</label>
-    <input type="text" id="inputId" />
+    <div style="margin-bottom: 15px;">
+      <label for="inputId">Input</label>
+      <input type="text" id="inputId" placeholder="placeholder" />
+    </div>
+    <div>
+      <label for="inputIdError">Input error</label>
+      <input type="text" id="inputIdError" required minlength="3" />
+      <div class="error">Value to short</div>
+    </div>
   `,
   styles: ``,
 })
